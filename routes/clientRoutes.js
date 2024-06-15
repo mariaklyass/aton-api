53e790d281e6565a67fa04f964874d4dec3eae8d
mariaklyass <klyassmaria@gmail.com>
2024-06-16 02:08:45 +0300

    fix: remove comments

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -5,9 +5,8 @@ const clientsController = require("../controllers/clientsController");
 router
   .route("/")
   .get(clientsController.getAllClients)
-  // .post(clientsController)
   .patch(clientsController.updateClientStatus);
-// .delete(clientsController);
+
 router.route("/:login").get(clientsController.getClientsByUser);
 
 module.exports = router;
