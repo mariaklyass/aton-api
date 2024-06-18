22ca6c372bc9b4f952d4f18505a8b7870a6e17f4
mariaklyass <klyassmaria@gmail.com>
2024-06-18 19:19:58 +0300

    fix: correct typo

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   "http://localhost:3500",
   "http://localhost:3000",
-  "https://aton-internship.vercel.app/",
+  "https://aton-internship.vercel.app",
 ];
 
 module.exports = allowedOrigins;
